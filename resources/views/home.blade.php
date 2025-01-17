6b700a826e686992e3dd47250fdaf40e88982d7c
Kagrtavy <nesterivan97@gmail.com>
2025-01-17 13:44:55 +0200

    changes with css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,12 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
-                <div class="grid grid-cols-5 gap-4"> <!-- Додано 5 колонок -->
+                <div class="grid grid-cols-5 gap-4">
                     @foreach ($publications as $publication)
                         <div class="bg-gray-100 dark:bg-gray-700 p-4 rounded-lg shadow-md book-card">
                             <img src="{{ asset('storage/' . $publication->photo) }}"
                                  alt="{{ $publication->name }}"
-                                 class="w-full h-48 object-cover rounded-md">
+                                 class="w-full h-48 object-cover rounded-md book-cover">
                             <h3 class="mt-4 text-sm font-semibold text-gray-800 dark:text-gray-200">
                                 {{ $publication->name }}
                             </h3>
